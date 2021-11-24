4d20e22864c0791fe1e0322d383cf99c961a06d7
seadiv <animesh1836@gmail.com>
2021-11-24 11:27:56 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Purpose:Football Scoreboard that can be interacted with in relation to updating data
+//Skills:Object Oriented Coding 
+#include <iostream>
+#include <unistd.h>
+#include <iomanip>
+#include "scoreboard.h"
+//use of header file for a cleaner method of main file
+using namespace std;
+int main()
+{
+	scoreboardEdit();
+	return 0;
+}
